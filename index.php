8ce37a9b73f507ff11b18655db91d3b5ce9d6ca5
AlexChernov <mikstik@yandex.ru>
2018-11-10 10:57:12 +0300

    v0.0 Creating Files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Добро пожаловать в LShop!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <script src="script.js"></script>
+</body>
+</html>
