4e9285b5031c4a5637b6e9361bddd46506e60b6c
AlexChernov <mikstik@yandex.ru>
2018-11-10 15:26:03 +0300

    design v1.0 Navbar

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <!DOCTYPE HTML>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <title>Добро пожаловать в LShop!</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-    Text Design
+ <header>
+    <div class="header_logo"><p>LShop</p></div>
+        <nav>
+            <div class="topnav" id="myTopnav">
+                <a href="#">Главная</a>
+                <a href="#">Каталог</a>
+                <a href="#">Контакты</a>
+                <a href="#">Отзывы</a>
+                <a href="#">Корзина</a>
+                <a href="#" id="menu" class="icon">&#9776</a>                
+            </div>
+        </nav>
+ </header>
     <script src="script.js"></script>
 </body>
 </html>
