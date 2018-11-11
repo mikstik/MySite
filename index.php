f5c15baa295b045db9fef251ef29300b74828c07
AlexChernov <mikstik@yandex.ru>
2018-11-11 20:51:24 +0300

    v1.2.

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Добро пожаловать в LShop!</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
  <header>
@@ -16,10 +17,49 @@
                 <a href="#">Контакты</a>
                 <a href="#">Отзывы</a>
                 <a href="#">Корзина</a>
+                <a href="#">Регистрация</a>
+                <a href="#">Вход</a>
                 <a href="#" id="menu" class="icon">&#9776</a>                
             </div>
-        </nav>
+        </nav>       
  </header>
-    <script src="script.js"></script>
+    <div class="stock">
+        <p><img src="/img/mainpicture.jpg" class="discount"></p>
+    </div>
+<div>
+    <div class="container-fluid">
+        <div class="row" id="searchform">
+            <div class="col-9">
+                <p>Вы можете воспользоваться поиском по нашему сайту: </p>
+            </div>
+                <div class="col-3">
+                    <div class="d1">
+                        <form>
+                        <input type="text" placeholder="Искать здесь...">
+                        <button type="submit"></button>
+                        </form>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+    <br>
+    <div class="productclass">
+    <div class="container-fluid" id="productform">
+            <div class="row">
+                <div class="col">
+                    <p>Текст 1</p>
+                </div>
+                <div class="col-8">
+                  <p>Текст 2</p>
+                </div>
+                <div class="col">
+                   <p>Текст 3</p>
+                </div>    
+            </div>
+        <div>
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="/js/script.js"></script>
 </body>
 </html>
